1f1d68bfdb11558c8cbb086e6aa3269bcc2ca40d
Michael Osoro <osoromichael@gmail.com>
2023-01-10 15:08:54 +0300

    update : update the homepage with graphical representation

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  days: any = [
+    {
+      day :'M',
+      percentage : '50%',
+      class:0
+    },{
+      day :'T',
+      percentage : '30%',
+      class:1
+    },{
+      day :'W',
+      percentage : '90%',
+      class:0
+    },{
+      day :'T',
+      percentage : '40%',
+      class:0
+    },{
+      day :'F',
+      percentage : '60%',
+      class:1
+    },{
+      day :'S',
+      percentage : '80%',
+      class:1
+    },{
+      day :'S',
+      percentage : '70%',
+      class:0
+    },
+  ];
+  counter: number = 1;
 
 }
