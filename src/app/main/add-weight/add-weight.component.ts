481911ac083d6c8548fe692c104f9158936a16d9
Michael Osoro <osoromichael@gmail.com>
2023-01-10 19:15:16 +0300

    update : updated UI elements and scroll on add-weight component

--- src/app/main/add-weight/add-weight.component.ts
+++ src/app/main/add-weight/add-weight.component.ts
@@ -1,14 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-add-weight',
   templateUrl: './add-weight.component.html',
   styleUrls: ['./add-weight.component.scss']
 })
-export class AddWeightComponent {
+export class AddWeightComponent implements OnInit {
   gender: string | undefined;
+  days: any = Array.from(Array(40).keys());
+  currentWeight = 56.4;
 
   updateGender(gender: string) {
     this.gender = gender;
+  }
+
+  onScroll($event: any) {
+    let elem = $event.currentTarget.scrollLeft;
+    this.currentWeight = elem;// >= 2 ? elem / 10 : elem;
+  }
+
+  ngOnInit() {
+    let elem = document.querySelector('.bars');
+    elem.scrollLeft += this.currentWeight;
+    this.scrollToCurrentDiv()
+  }
+
+
+  scrollToCurrentDiv() {
+    console.warn();
   }
 }
