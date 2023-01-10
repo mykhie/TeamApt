48cbb8a71c6eb570c18053f0e221a8dea6498aec
Michael Osoro <osoromichael@gmail.com>
2023-01-10 09:53:13 +0300

    update : updatedhistory graph for sleep

--- src/app/main/history/history.component.ts
+++ src/app/main/history/history.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent {
+  days: any = [
+    {
+      day :'M',
+      percentage : '50%',
+      class:0
+    },{
+      day :'T',
+      percentage : '10%',
+      class:1
+    },{
+      day :'W',
+      percentage : '90%',
+      class:0
+    },{
+      day :'T',
+      percentage : '40%',
+      class:0
+    },{
+      day :'F',
+      percentage : '60%',
+      class:1
+    },{
+      day :'S',
+      percentage : '80%',
+      class:1
+    },{
+      day :'S',
+      percentage : '70%',
+      class:0
+    },
+  ];
 
 }
