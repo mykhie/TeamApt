476966e601430c5fd43f6311bf37e5fbef83ec5b
Michael Osoro <osoromichael@gmail.com>
2023-01-11 20:51:19 +0300

    fix : issue on undefined native element

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -10,13 +10,11 @@ export class ProfileComponent extends BaseComponent implements AfterViewInit {
 
   @ViewChild('calories') c: any;
   @ViewChild('steps') s: any;
-  @ViewChild('mins') m: any;
   @ViewChild('weight') w: any;
   @ViewChild('heartbeat') h: any;
   ngAfterViewInit() {
     this.animateValue(this.c, 0, 982, 2000);
     this.animateValue(this.s, 0, 2930, 2000);
-    this.animateValue(this.m, 0, 650, 2000);
     this.animateValue(this.w, 0, 89, 2000);
     this.animateValue(this.h, 0, 123, 2000);
   }
