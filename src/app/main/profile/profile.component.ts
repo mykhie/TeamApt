77f050f0bf774e1381619579b79548b552e1f0fd
Michael Osoro <osoromichael@gmail.com>
2023-01-11 19:06:39 +0300

    update : added animations on profile

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {BaseComponent} from "@app//common/base/base.component";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent extends BaseComponent implements AfterViewInit {
 
+  @ViewChild('calories') c: any;
+  @ViewChild('steps') s: any;
+  @ViewChild('mins') m: any;
+  @ViewChild('weight') w: any;
+  @ViewChild('heartbeat') h: any;
+  ngAfterViewInit() {
+    this.animateValue(this.c, 0, 982, 2000);
+    this.animateValue(this.s, 0, 2930, 2000);
+    this.animateValue(this.m, 0, 650, 2000);
+    this.animateValue(this.w, 0, 89, 2000);
+    this.animateValue(this.h, 0, 123, 2000);
+  }
 }
